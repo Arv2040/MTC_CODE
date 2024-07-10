74a3341d6c8c7a5811af4455a9f4dfe7eaa84420
Arv2040 <aravcr4085@gmail.com>
2024-07-11 01:45:14 +0530

    feat: added multimodal integration

--- src/cf_vector.py
+++ src/cf_vector.py
@@ -35,21 +35,27 @@ client = AzureOpenAI(
 )
 # very very important code here-------------- dont touch---------
 document_client = DocumentAnalysisClient(os.getenv("doc_endpoint"), AzureKeyCredential(os.getenv("doc_apikey")))
-main_folder_path = r"C:\Users\A J\Desktop\MTC_CODE\Datasets"
+# main_folder_path = r"your path"
 fraud_detection_folder = os.path.join(main_folder_path, "Fraud_Detection")
 fraud_directory = next(os.walk(fraud_detection_folder))
 current_directory, directories, files = fraud_directory
 entries = os.listdir(fraud_detection_folder)
+d = getdatafromblob('Structured_Data.json','corporatefraud')
+d = json.loads(d)
+document_text_final = []
+image_final = []
+text_final = []
 for dir in entries:
 #    createcontainer((dir))
     sub_dir_path = os.path.join(fraud_detection_folder,dir)
+    print(sub_dir_path)
     for root,dirs,files in os.walk(sub_dir_path):
         for file in files:
-            with open(sub_dir_path,"rb") as data:
+            with open(os.path.join(sub_dir_path,file),"rb") as data:
                 uploaddata(file,dir,data)
         break
     blob_list = getbloblist(dir)
-    print(blob_list)
+    
     document_text_list = []
     image_list = []
     text_list = []
@@ -71,10 +77,36 @@ for dir in entries:
             document_text_list.append(full_text)
         else:
             text_content = getdatafromblob(blob.name,dir)
+            text_content = text_content.decode('utf-8')
             text_list.append(text_content)
+    document_text_final.append(document_text_list)
+    image_final.append(image_list)
+    text_final.append(text_list)
+    
 #------------------------------------------------------------------
-
-
+for i in range(len(d)):
+    element = d[i]
+    element['document_text'] = 'a'
+    element['images'] = 'Images- '
+    element['txt_text'] = 'a'
+    for document_array in document_text_final:
+        for document in document_array:
+            element['document_text'] += document
+    for image_array in image_final:
+        for image in image_array:
+            element['images'] += f"{image},"
+    for text_array in text_final:
+        for text in text_array:
+            element['txt_text'] += text 
+# d = json.dumps(d)
+# uploaddata("checkthis.json","unstructureddata",d)
+        # if f"CompanyID = {str(i)}" in document:
+        #     for customer in data:
+        #         customer['document_text'] = "a"
+        #         if(customer['CompanyID'] == str(i)):
+        #             customer['document_text'] += document
+data = d
+print("done")
 
 
 
@@ -122,7 +154,7 @@ for dir in entries:
 #     document_list.append(full_text)
 
 # data = getdatafromblob("corporate_dataset.json", os.getenv("CONTAINER_NAME_FRAUD"))
-# data = json.loads(data)
+
 
 # #mapped structured and unstructured data
 
@@ -135,71 +167,71 @@ for dir in entries:
 #                     customer['document_text'] += document
 
 
-# dataitem = data[0]
-# data_dict = dict(dataitem)
-# key_list = list(data_dict.keys())
+dataitem = data[0]
+data_dict = dict(dataitem)
+key_list = list(data_dict.keys())
 
-# #CREATING THE INDEX
-# index_name_fraud = os.getenv("corporate-index9")
-# search_client = SearchIndexClient(os.getenv("service_endpoint"),AzureKeyCredential(os.getenv("admin_key")))
-# key_list = list(data_dict.keys())
+#CREATING THE INDEX
+index_name_fraud = "fraudindex5"
+search_client = SearchIndexClient(os.getenv("service_endpoint"),AzureKeyCredential(os.getenv("admin_key")))
+key_list = list(data_dict.keys())
 
-# fields = [
-#     SearchableField(name=f"{str(key_list[0])}", type=SearchFieldDataType.String, key=True, sortable=True, filterable=True, facetable=True),
-# ]
-# for i in range(1, len(key_list),1):
-#     element = SearchableField(name=f"{str(key_list[i])}", type=SearchFieldDataType.String, sortable=True, filterable=True, facetable=True)
-#     fields.append(element)
-# for i in range(len(key_list)):
-#     element = SearchField(name=f"{str(key_list[i])}_Vector", type=SearchFieldDataType.Collection(SearchFieldDataType.Single), searchable=True, vector_search_dimensions=1536, vector_search_profile_name="myHnswProfile")
-#     fields.append(element)
+fields = [
+    SearchableField(name=f"{str(key_list[0])}", type=SearchFieldDataType.String, key=True, sortable=True, filterable=True, facetable=True),
+]
+for i in range(1, len(key_list),1):
+    element = SearchableField(name=f"{str(key_list[i])}", type=SearchFieldDataType.String, sortable=True, filterable=True, facetable=True)
+    fields.append(element)
+for i in range(len(key_list)):
+    element = SearchField(name=f"{str(key_list[i])}_Vector", type=SearchFieldDataType.Collection(SearchFieldDataType.Single), searchable=True, vector_search_dimensions=1536, vector_search_profile_name="myHnswProfile")
+    fields.append(element)
 
-# names_lists = []
-# response_lists = []
-# embedding_lists = []
-# for i in range(len(key_list)):
-#     names_list = [str(dataitem[f'{key_list[i]}']) for dataitem in data]
-#     response_list = client.embeddings.create(input=names_list, model = os.getenv("azure_openai_em_name"))
-#     embedding_list = [item.embedding for item in response_list.data]
-#     names_lists.append(names_list)
-#     response_lists.append(response_list)
-#     embedding_lists.append(embedding_list)
+names_lists = []
+response_lists = []
+embedding_lists = []
+for i in range(len(key_list)):
+    names_list = [str(dataitem[f'{key_list[i]}']) for dataitem in data]
+    response_list = client.embeddings.create(input=names_list, model = os.getenv("azure_openai_em_name"))
+    embedding_list = [item.embedding for item in response_list.data]
+    names_lists.append(names_list)
+    response_lists.append(response_list)
+    embedding_lists.append(embedding_list)
 
-# for i, dataitem in enumerate(data):
-#     for j, key in enumerate(key_list):
-#         dataitem[f'{key}_Vector'] = embedding_lists[j][i]
+for i, dataitem in enumerate(data):
+    for j, key in enumerate(key_list):
+        dataitem[f'{key}_Vector'] = embedding_lists[j][i]
 
-# # Open the local file and upload its contents
-# data_string = json.dumps(data)
-# uploaddata('llminputdata.json', os.getenv("CONTAINER_NAME_FRAUD"), data_string)
+# Open the local file and upload its contents
+data_string = json.dumps(data)
+uploaddata('llminputdata.json', os.getenv("CONTAINER_NAME_FRAUD"), data_string)
 
-# #defining the vector search algorithm
-# vector_search = VectorSearch(
-#     algorithms=[
-#         HnswAlgorithmConfiguration(
-#             name="myHnsw"
-#         )
-#     ],
-#     profiles=[
-#         VectorSearchProfile(
-#             name="myHnswProfile",
-#             algorithm_configuration_name="myHnsw",
-#         )
-#     ]
-# )
+#defining the vector search algorithm
+vector_search = VectorSearch(
+    algorithms=[
+        HnswAlgorithmConfiguration(
+            name="myHnsw"
+        )
+    ],
+    profiles=[
+        VectorSearchProfile(
+            name="myHnswProfile",
+            algorithm_configuration_name="myHnsw",
+        )
+    ]
+)
 
-# # Create the search index and defining the algorithm we previously created
-# index = SearchIndex(name="corporate-index9", fields=fields, vector_search=vector_search)
-# result = search_client.create_or_update_index(index)
-# print(f'{result.name} created')
+# Create the search index and defining the algorithm we previously created
+index = SearchIndex(name="fraudindex5", fields=fields, vector_search=vector_search)
+result = search_client.create_or_update_index(index)
+print(f'{result.name} created')
 
-# # Getting the mapped and embedded data from the blob
-# prefinal_data = getdatafromblob('llminputdata.json', os.getenv("CONTAINER_NAME_FRAUD"))
-# json_data = json.loads(prefinal_data)
+# Getting the mapped and embedded data from the blob
+prefinal_data = getdatafromblob('llminputdata.json', os.getenv("CONTAINER_NAME_FRAUD"))
+json_data = json.loads(prefinal_data)
 
-# # Upload the documents to the vector store
-# search_client = SearchClient(endpoint=os.getenv("service_endpoint"), index_name="corporate-index9", credential=AzureKeyCredential(os.getenv("admin_key")))
-# results = search_client.upload_documents(json_data)
-# print(f"Uploaded {len(json_data)} documents")
+# Upload the documents to the vector store
+search_client = SearchClient(endpoint=os.getenv("service_endpoint"), index_name="fraudindex5", credential=AzureKeyCredential(os.getenv("admin_key")))
+results = search_client.upload_documents(json_data)
+print(f"Uploaded {len(json_data)} documents")
 
 
